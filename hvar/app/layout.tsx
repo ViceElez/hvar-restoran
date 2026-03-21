e536207e70812d1045fb2f8917904e45fc24afac
ViceElez <vice.elezovic12@gmail.com>
2026-03-21 21:31:06 +0100

    add analytics

--- hvar/app/layout.tsx
+++ hvar/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import HamburgerMenu from "./components/HamburgerMenu";
 import FooterSection from "@/app/components/FooterSection";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <HamburgerMenu />
         {children}
         <FooterSection />
+        <Analytics />
       </body>
     </html>
   );
